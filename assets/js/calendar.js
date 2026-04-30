90aa81fbd994296b4830a91c8efc686929668445
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-04-30 09:27:18 +0200

    feat: add monthly view

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,13 +1,40 @@
+lucide.createIcons();
+
 const calenderItemSection = document.querySelector(".calender-item--section");
+const calenderMonthSection = document.querySelector(".calender-month--section");
 
-for (let i = 0; i < 31; i++) {
-  let calenderItem = document.createElement("article");
-  calenderItem.classList.add("calender--item");
+const leftChevron = document.querySelector(".left-arrow");
+const rightChevron = document.querySelector(".right-arrow");
 
-  calenderItem.textContent = [i + 1];
+let currentMonth = new Date().getMonth();
 
-  calenderItemSection.appendChild(calenderItem);
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function renderCalender() {
+  calenderItemSection.innerHTML = "";
+
+  const monthName = new Date(2026, currentMonth).toLocaleString("sv-SE", {
+    month: "long",
+  });
+  const monthTitle = document.querySelector(".month-title");
+  monthTitle.classList.add("calender-month--headline");
+
+  monthTitle.textContent = monthName;
+
+  const inc = daysInMonth[currentMonth];
+
+  for (let i = 0; i < inc; i++) {
+    let calenderItem = document.createElement("article");
+    calenderItem.classList.add("calender--item");
+
+    calenderItem.textContent = [i + 1];
+
+    calenderItemSection.appendChild(calenderItem);
+  }
 }
+
+renderCalender();
 
 const calenderItems = document.querySelectorAll(".calender--item");
 
@@ -16,3 +43,13 @@ for (let i = 0; i < calenderItems.length; i++) {
     e.target.style.background = "white";
   });
 }
+
+rightChevron.addEventListener("click", () => {
+  currentMonth = (currentMonth + 1) % 12;
+  renderCalender();
+});
+
+leftChevron.addEventListener("click", () => {
+  currentMonth = (currentMonth - 1 + 12) % 12;
+  renderCalender();
+});
