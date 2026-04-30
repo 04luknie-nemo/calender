1e23b9336e9c446f6a01035364dbfb8d1d51d8ca
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-04-30 11:44:56 +0200

    fix aside error

--- assets/js/aside-top.js
+++ assets/js/aside-top.js
@@ -4,9 +4,6 @@ export function initClock() {
     clock(nu);
   }, 1000);
 
-  weekday(nu);
-  month(nu);
-
   function clock(nu) {
     const timeString = nu.toLocaleTimeString("sv", {
       hour: "numeric",
@@ -14,72 +11,77 @@ export function initClock() {
       second: "numeric",
     });
 
+    weekday(nu);
+    month(nu);
+
     const element = document.getElementById("time");
 
     if (element) {
       element.textContent = timeString;
     }
+
     function weekday(nu) {
-        const weekday = nu.toLocaleDateString("sv", {
-            weekday: "long"
-        });
-        document.getElementById("weekday").textContent = weekday;
+      const weekday = nu.toLocaleDateString("sv", {
+        weekday: "long",
+      });
+      document.getElementById("weekday").textContent = weekday;
     }
+
     function month(nu) {
-        const datum = nu.toLocaleDateString("sv", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-        });
-        document.getElementById("datum").textContent = datum;
+      const datum = nu.toLocaleDateString("sv", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      });
+      document.getElementById("datum").textContent = datum;
     }
 
     addTodo();
     const todoList = [];
-    
-    
+
     // Antingen ha addingTodo i addTodo ELLER hämta description och date i addingTodo
     function addTodo(todo) {
-        const addTodoForm = document.getElementById("addTodoForm");
-        
-        const fieldSet = document.getElementById("addTodo");
-        
-        const button = document.getElementById("submitButton");
-        addTodoForm.addEventListener("submit", addingTodo);
+      const addTodoForm = document.getElementById("addTodoForm");
+
+      const fieldSet = document.getElementById("addTodo");
+
+      const button = document.getElementById("submitButton");
+      addTodoForm.addEventListener("submit", addingTodo);
     }
-    
+
     function addingTodo(event) {
-        event.preventDefault();
-        
-        const description = document.getElementById("description");
-        
-        const todoDate = new Date(document.getElementById("dateToDo").value);
-        
-        const nu = new Date();
-        
-        if (nu > todoDate) {
-            console.error("För tidigt datum");
-            return;
-        }
-        const todoObject = {
-            description: description.value,
-            date: todoDate
-        }
-        console.log(description.value);
-        console.log(todoDate);
-        
-        todoList.push(todoObject);
-        renderTodoList(todoList);
-        console.log(todoList);
+      event.preventDefault();
+
+      const description = document.getElementById("description");
+
+      const todoDate = new Date(document.getElementById("dateToDo").value);
+
+      const nu = new Date();
+
+      if (nu > todoDate) {
+        console.error("För tidigt datum");
+        return;
+      }
+      const todoObject = {
+        description: description.value,
+        date: todoDate,
+      };
+      console.log(description.value);
+      console.log(todoDate);
+
+      todoList.push(todoObject);
+      renderTodoList(todoList);
+      console.log(todoList);
     }
 
     function renderTodoList(todoList) {
-        const list = document.getElementById("todo-list");
-        list.innerHTML = "";
-        todoList.forEach((todo) => {
-            const p = document.createElement("p");
-            p.textContent = todo.description;
-            document.getElementById("todo-list").appendChild(p);
-        })
+      const list = document.getElementById("todo-list");
+      list.innerHTML = "";
+      todoList.forEach((todo) => {
+        const p = document.createElement("p");
+        p.textContent = todo.description;
+        document.getElementById("todo-list").appendChild(p);
+      });
     }
+  }
 }
