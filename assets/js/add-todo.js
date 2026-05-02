3d30b6194e1999e3cd8df99fbd13a2a44e34fcff
Lukas <04luknie@skola.boras.se>
2026-05-02 08:16:02 +0200

    Restructured form related code, right now only add-todo file and "main" form file for rendering and so on

---
+++ assets/js/add-todo.js
@@ -0,0 +1,22 @@
+document.getElementById("addTodoForm").addEventListener("submit", addingTodo);
+
+function addingTodo(event) {
+  event.preventDefault();
+  const description = document.getElementById("description");
+  const todoDate = new Date(document.getElementById("dateToDo").value);
+  const nu = new Date();
+
+  if (nu > todoDate) {
+    console.error("För tidigt datum");
+    return;
+  }
+  const todoObject = {
+    description: description.value,
+    date: todoDate,
+  };
+
+  todoList.push(todoObject);
+  renderTodoList(todoList);
+  addTodoForm.classList.toggle("hidden");
+  event.target.reset();
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,54 +1,16 @@
-// Antingen ha addingTodo i addTodo ELLER hämta description och date i addingTodo
 const todoList = [];
-
 const showForum = document.getElementById("show-forum-button");
 showForum.addEventListener("click", displayForum);
-const addTodoForm = document.getElementById("addTodoForm");
 
 function displayForum() {
-  addTodoForm.classList.toggle("hidden");
+  document.getElementById("addTodoForm").classList.toggle("hidden")
 }
-
-function addTodo(todo) {
-  const fieldSet = document.getElementById("addTodo");
-
-  const button = document.getElementById("submitButton");
-  addTodoForm.addEventListener("submit", addingTodo);
-}
-
-function addingTodo(event) {
-  event.preventDefault();
-
-  const description = document.getElementById("description");
-
-  const todoDate = new Date(document.getElementById("dateToDo").value);
-
-  const nu = new Date();
-
-  if (nu > todoDate) {
-    console.error("För tidigt datum");
-    return;
-  }
-  const todoObject = {
-    description: description.value,
-    date: todoDate,
-  };
-  console.log(description.value);
-  console.log(todoDate);
-
-  todoList.push(todoObject);
-  renderTodoList(todoList);
-  console.log(todoList);
-  addTodoForm.classList.toggle("hidden");
-  event.target.reset();
-}
-
 function renderTodoList(todoList) {
   const list = document.getElementById("todo-list");
   list.innerHTML = "";
   todoList.forEach((todo) => {
-    const p = document.createElement("p");
-    p.textContent = todo.description;
-    document.getElementById("todo-list").appendChild(p);
+    const li = document.createElement("li");
+    li.textContent = todo.description;
+    list.appendChild(li);
   });
 }
