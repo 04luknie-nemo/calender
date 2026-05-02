5b2f07b743c3703eab50e8e4d882d5b646f318f6
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-05-02 09:28:59 +0200

    feat: add holidays to calender view

--- assets/js/holiday.js
+++ assets/js/holiday.js
@@ -1,12 +1,22 @@
 class Holiday {
-  constructor(name, day, month) {
+  constructor(name, month, day) {
     this.name = name;
-    this.day = day;
     this.month = month;
+    this.day = day;
   }
 }
 
 export const holidays = [
-  new Holiday("New Year", 1),
-  new Holiday("Midsummer", 6),
+  new Holiday("Nyårsdagen", 1, 1),
+  new Holiday("Trettondedag jul", 1, 6),
+  new Holiday("Långfredagen", 4, 3),
+  new Holiday("Påskdagen", 4, 5),
+  new Holiday("Annandag påsk", 4, 6),
+  new Holiday("Första maj", 5, 1),
+  new Holiday("Kristi himmelsfärdsdag", 5, 14),
+  new Holiday("Pingstdagen", 5, 24),
+  new Holiday("Sveriges nationaldag", 6, 6),
+  new Holiday("Alla helgons dag", 11, 1),
+  new Holiday("Juldagen", 12, 25),
+  new Holiday("Annandag jul", 12, 26),
 ];
